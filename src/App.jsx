acb428f6f4ef87f68eef8b867e9c1e1a28d80045
Kannan <kbkannan2001@gmail.com>
2024-12-03 21:09:07 +0530

    version 1 of my project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "../src/bootstrap.min.css";
+import "./App.css";
+function App() {
+  return (
+    <>
+
+
+      <div className="d-flex justify-content-center align-items-center">
+
+        <div>
+          <h1 className="text-center">Hello this is a sample project</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
